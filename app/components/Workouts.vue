e9206b72ebac4aa06ac206c8ed8bb5e8aaf6d21c
Brandon <brandon@brandon.land>
2021-09-01 19:04:10 -0400

    add Workouts component

---
+++ app/components/Workouts.vue
@@ -0,0 +1,26 @@
+<template>
+  <Page>
+    <ActionBar :title="message" />
+
+    <GridLayout>
+      <Label class="info">
+        <FormattedString>
+          <Span class="fas" text.decode="&#xf135; " />
+          <Span :text="message" />
+        </FormattedString>
+      </Label>
+    </GridLayout>
+  </Page>
+</template>
+
+<script>
+export default {
+  computed: {
+    message() {
+      return "Exercises";
+    }
+  }
+};
+</script>
+
+<style></style>
