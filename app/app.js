99dd92046b432837d9b8e8a365fee5ba0961eca1
Brandon <brandon@brandon.land>
2021-09-01 18:49:29 -0400

    add Vue Devtools for development workflow

--- app/app.js
+++ app/app.js
@@ -1,7 +1,11 @@
-import Vue from 'nativescript-vue'
+import Vue from "nativescript-vue";
+import VueDevtools from "nativescript-vue-devtools";
 
-import Home from './components/Home'
+import Home from "./components/Home";
+
+// let env = process.env.NODE_ENV || 'development';
+Vue.use(VueDevtools);
 
 new Vue({
-  render: (h) => h('frame', [h(Home)]),
-}).$start()
+  render: h => h("frame", [h(Home)])
+}).$start();
