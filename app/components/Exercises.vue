7644e8d9d95f4c7c0f668a2eb3f4724852e6a1ff
Brandon <brandon@brandon.land>
2021-09-01 18:30:06 -0400

    add new page/view

---
+++ app/components/Exercises.vue
@@ -0,0 +1,28 @@
+<template>
+  <Page>
+    <ActionBar>
+      <Label text="Exercises" />
+    </ActionBar>
+
+    <GridLayout>
+      <Label class="info">
+        <FormattedString>
+          <Span class="fas" text.decode="&#xf135; " />
+          <Span :text="message" />
+        </FormattedString>
+      </Label>
+    </GridLayout>
+  </Page>
+</template>
+
+<script>
+export default {
+  computed: {
+    message() {
+      return "Exercises";
+    }
+  }
+};
+</script>
+
+<style></style>
