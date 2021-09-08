bccc616cc53e5ce28a7529491db811b6ea2fc24c
Brandon <brandon@brandon.land>
2021-09-08 19:26:54 -0400

    add some mock-up data and display pages in gridlayout on homepage

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -10,22 +10,72 @@
             <Span :text="message" />
           </FormattedString>
         </Label>
-        <Button text="Exercises" @tap="$navigateTo(exercisesPage)" />
-        <Button text="Workouts" @tap="$navigateTo(workoutsPage)" />
-        <Button text="Analysis" @tap="$navigateTo(analysisPage)" />
-        <Button text="BodyTracker" @tap="$navigateTo(bodyTrackerPage)" />
-        <Button text="Calendar" @tap="$navigateTo(calendarPage)" />
-        <Button text="Goals" @tap="$navigateTo(goalsPage)" />
-        <Button text="Records" @tap="$navigateTo(recordsPage)" />
-        <Button text="Settings" @tap="$navigateTo(settingsPage)" />
+        <GridLayout columns="*, *, *" rows="auto, auto, auto">
+          <Button
+            text="Workouts"
+            @tap="$navigateTo(links.Workouts)"
+            height="100"
+            row="0"
+            column="0"
+          />
+          <Button
+            text="Exercises"
+            @tap="$navigateTo(links.Exercises)"
+            height="100"
+            row="0"
+            column="1"
+          />
+          <Button
+            text="Analysis"
+            @tap="$navigateTo(links.Analysis)"
+            height="100"
+            row="0"
+            column="2"
+          />
+          <Button
+            text="BodyTracker"
+            @tap="$navigateTo(links.BodyTracker)"
+            height="100"
+            row="1"
+            column="0"
+          />
+          <Button
+            text="Calendar"
+            @tap="$navigateTo(links.Calendar)"
+            height="100"
+            row="1"
+            column="1"
+          />
+          <Button
+            text="Goals"
+            @tap="$navigateTo(links.Goals)"
+            height="100"
+            row="1"
+            column="2"
+          />
+          <Button
+            text="Records"
+            @tap="$navigateTo(links.Records)"
+            height="100"
+            row="2"
+            column="0"
+          />
+          <Button
+            text="Settings"
+            @tap="$navigateTo(links.Settings)"
+            height="100"
+            row="2"
+            column="1"
+          />
+        </GridLayout>
       </StackLayout>
     </GridLayout>
   </Page>
 </template>
 
 <script>
-import Exercises from "./Exercises";
 import Workouts from "./Workouts";
+import Exercises from "./Exercises";
 import Analysis from "./Analysis";
 import BodyTracker from "./BodyTracker";
 import Calendar from "./Calendar";
@@ -37,24 +87,152 @@ export default {
   name: "Home",
   components: {
     Exercises,
-    Workouts
+    Workouts,
+    Analysis,
+    BodyTracker,
+    Goals,
+    Records,
+    Settings
   },
   data() {
     return {
-      exercisesPage: Exercises,
-      workoutsPage: Workouts,
-      analysisPage: Analysis,
-      bodyTrackerPage: BodyTracker,
-      calendarPage: Calendar,
-      goalsPage: Goals,
-      recordsPage: Records,
-      settingsPage: Settings
+      links: {
+        Exercises,
+        Workouts,
+        Analysis,
+        BodyTracker,
+        Goals,
+        Records,
+        Settings
+      },
+      pages: [
+        {
+          id: 0,
+          name: "Exercises",
+          component: Exercises,
+          items: [
+            {
+              id: 1,
+              name: "Barbell Squat",
+              type: "resistance",
+              region: "lower_body",
+              muscleGroup: "lower_body",
+              muscles: ["quadriceps", "glutimus_maximus", "hamstrings"]
+            },
+            {
+              id: 2,
+              name: "Deadlift",
+              type: "resistance",
+              region: "lower_body",
+              settings: {
+                weight_kg: 0.0,
+                weight_lbs: 0.0,
+                reps: 0
+              },
+              muscleGroup: "lower_body",
+              muscles: ["quadriceps", "glutimus_maximus", "lower_back"]
+            },
+            {
+              id: 3,
+              name: "Leg Press",
+              type: "resistance",
+              region: "lower_body",
+              muscleGroup: "legs",
+              muscles: ["quadriceps", "glutimus_maximus", "hamstrings"],
+              settings: {
+                weight_kg: 0.0,
+                weight_lbs: 0.0,
+                reps: 0
+              }
+            },
+            {
+              id: 4,
+              name: "Barbell Bench Press",
+              type: "resistance",
+              region: "upper_body",
+              muscleGroup: "chest",
+              muscles: ["chest", "triceps"],
+              settings: {
+                weight_kg: 0.0,
+                weight_lbs: 0.0,
+                reps: 0
+              }
+            },
+            {
+              id: 5,
+              name: "Treadmill",
+              type: "cardio",
+              settings: {
+                incline: 0.0,
+                duration: 0.0, // seconds
+                distance_km: 0.0,
+                distance_mi: 0.0
+              }
+            },
+            {
+              id: 6,
+              name: "Standing Dumbbell Curls",
+              type: "resistance",
+              region: "upper_body",
+              muscleGroup: "arms",
+              settings: {
+                weight_kg: 0.0,
+                weight_lbs: 0.0,
+                reps: 0
+              }
+            }
+          ]
+        },
+        {
+          id: 1,
+          name: "Workouts",
+          component: Workouts
+        },
+        {
+          id: 2,
+          name: "Analysis",
+          component: Analysis
+        },
+        {
+          id: 3,
+          name: "BodyTracker",
+          component: BodyTracker
+        },
+        {
+          id: 4,
+          name: "Calendar",
+          component: Calendar
+        },
+        {
+          id: 5,
+          name: "Goals",
+          component: Goals
+        },
+        {
+          id: 6,
+          name: "Records",
+          component: Records
+        },
+        {
+          id: 7,
+          name: "Settings",
+          component: Settings
+        }
+      ]
     };
   },
   methods: {
     isHomePage() {
       return true;
       // this.$
+    },
+    onItemTap(e) {
+      console.log("Tapped an item!");
+      console.log(e.index);
+      console.log(e.item);
+    },
+    getComponentByName(compName) {
+      return this.components.filter(c => c.name === compName);
     }
   },
   computed: {
